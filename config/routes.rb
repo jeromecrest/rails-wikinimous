e3f1eea683cf8962a65b0e6d9678133ee869d631
Jerome Crest <jerome.crest@gmail.com>
2020-03-03 16:30:00 +0100

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  resources :articles, only: [:create, :index, :destroy, :new, :update, :show, :edit]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
